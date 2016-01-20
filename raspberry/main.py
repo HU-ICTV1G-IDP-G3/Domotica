28c59d903e5e0846d4b1110ee7d202ff032b35c9
kasumi <kasumi@kasumi-owari.moe>
2016-01-20 15:17:47 +0000

    Update lights depending on the database

--- raspberry/main.py
+++ raspberry/main.py
@@ -6,19 +6,52 @@ import config
 
 import time
 
+conf = {}
+
 def Main():
+    global conf
     conf = config.LoadConfig()
 
     raspberry.SetupRaspberry()
     database.SetDatabaseCredentials(conf['host'], conf['user'], conf['password'], conf['database'])
 
     database.ConnectDatabase()
-    database.GetLightStatus()
+    UpdateLights()
     database.DisconnectDatabase()
 
     while True:
+        database.ConnectDatabase()
+
         if raspberry.UpdateHelpButton():
             print "Help Button Pressed"
+        UpdateLights()
+
+        database.DisconnectDatabase()
+
+        time.sleep(0.1)
+
+
+def UpdateLights():
+    global conf
+
+    lightstatus = database.GetLightStatus()
+    for light in lightstatus:
+        lightOn = False
+        if light['turnedon'] != 0:
+            lightOn = True
+        if light['idLight'] == int(conf['light1']):
+            raspberry.SetLight1(lightOn)
+        if light['idLight'] == int(conf['light2']):
+            raspberry.SetLight2(lightOn)
+        if light['idLight'] == int(conf['light3']):
+            raspberry.SetLight3(lightOn)
 
 if __name__ == "__main__":
-    Main()
+    try:
+        Main()
+    except KeyboardInterrupt:
+        print "Disconnecting from database"
+        try:
+            database.DisconnectDatabase()
+        except Exception:
+            pass
