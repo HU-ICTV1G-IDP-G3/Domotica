4444f700ab3e09666745cedb65e98abb8ec4d76f
Matthias van Dijk <matthiasvd2@gmail.com>
2016-01-21 12:11:11 +0100

    Updated the admin page.

--- WebApplicatie/init.py
+++ WebApplicatie/init.py
@@ -5,11 +5,14 @@ from flask_kvsession import KVSessionExtension
 from simplekv.memory.redisstore import RedisStore
 from flask_wtf import Form
 
-from wtforms import PasswordField, validators, HiddenField, StringField
+from wtforms import PasswordField, validators, HiddenField, StringField, SelectField
 from wtforms import TextAreaField, BooleanField
 from wtforms.validators import EqualTo, Optional, data_required
 from wtforms.validators import Length, Email
 
+from passlib.handlers.sha2_crypt import sha256_crypt as hash
+
+#Importeert de redis functies voor later gebruik.
 store = RedisStore(redis.StrictRedis())
 
 app = Flask(__name__, static_url_path='/static')
@@ -18,34 +21,86 @@ app.config.update(
     SESSION_KEY_BITS=128
 )
 
+#Een form gegenereert voor de pagina admin.html
+#Hiermee kan een admin een nieuwe user toevoegen.
+class NieuweGebruikerForm(Form):
+     gebruikersnaam = StringField('Gebruikersnaam', validators=[
+           data_required('Voer een gebruikersnaam in.')])
+     password = PasswordField('Voer een veilig wachtwoord in.', validators=[
+           data_required('Voer een geldig wachtwoord in.')])
+     voornaam = StringField('Voornaam', validators=[
+           data_required('Dit veld is verplicht.'),
+           Length(min=2, max=30, message=(u'Uw voornaam moet minimaal 2 en mag maximaal 30 tekens bevatten.'))])
+     achternaam = StringField('Achternaam', validators=[
+           data_required('Dit veld is verplicht.'),
+           Length(min=2, max=30, message=(u'Uw achternaam moet minimaal 2 en mag maximaal 30 tekens bevatten.'))])
+     rang = SelectField('Rang', choices=[("1", "Bewoner"), ("2", "Meldkamer medewerker"), ("3", "Admin")], default="1")
+
+#Voor serverside sessions, maakt gebruik van redis. (Vergeet dus niet dat redis een vereiste is op de locatie waar je deze applicatie wilt draaien.)
 KVSessionExtension(store, app)
 
 # @app.before_request
 # def db_connect():
 #     g.db_conn = pymysql.connect(host='213.233.237.7',
 #                                  user='domotica',
-#                                  password='SecretPass :O :O :O',
+#                                  password='Secret so secret',
 #                                  db='domotica_db',
 #                                  charset='utf8',
 #                                  port=3306)
 #     global cur
 #     cur = g.db_conn.cursor()
 
-@app.route('/')
+
+#De index pagina, met de login staat hieronder vermeld:
+@app.route('/', methods=["GET", "POST"])
 def login():
     return render_template("login.html")
 
-@app.route('/bewoner/')
+
+#De bewoner pagina, met de verlichting functies staan hieronder vermeld:
+@app.route('/bewoner/', methods=["GET", "POST"])
 def bewoner():
     return render_template("bewoner.html")
 
-@app.route('/meldkamer/')
+
+#De meldkamer pagina, staat hieronder vermeld:
+@app.route('/meldkamer/', methods=["GET", "POST"])
 def meldkamer():
     return render_template("meldkamer.html")
 
-@app.route('/admin/')
+
+#De admin pagina, met de funcites om een nieuw account aan te maken en te beheren staat hieronder vermeld:
+@app.route('/admin/', methods=["GET", "POST"])
 def admin():
-    return render_template("admin.html")
+    #Vraagt het eerder gemaakte 'NieuweGebruikerForm' form aan.
+    form = NieuweGebruikerForm()
+
+    #Zodra de post op de pagina langs de vallidators van WTForm zijn gegaan kan de rest plaatsvinden.
+    if form.validate_on_submit():
+        #Nodig voor de request die we hierna gaan maken.
+        admin = NieuweGebruikerForm(request.form)
+
+        #Alle velden worden binnengehaald en aan een variabele gekoppelt.
+        username = admin.gebruikersnaam.data
+        password = admin.password.data
+        voornaam = admin.voornaam.data
+        achternaam = admin.achternaam.data
+        rang = admin.rang.data
+
+        #Een laatste check of de username wel beschikbaar is (Deze moet uniek zijn):
+        cur.execute("SELECT username FROM User WHERE username =%s", (escape(username)))
+        beschikbaar = cur.fetchall()
+
+        if beschikbaar == ():
+            #Het wachtwoord wordt met een random salt gehashed en beide worden in één variabele gestopt.
+            versleutelde_password = hash.encrypt(escape(password))
+
+            #De gegevens worden naar de database verstuurd:
+            cur.execute("INSERT INTO User (username, forename, lastname, password, rank) VALUES (%s, %s, %s, %s, %s)", (escape(username), escape(voornaam), escape(achternaam), versleutelde_password, int(rang)))
+
+
+
+    return render_template("admin.html", NieuweGebruikerForm=form)
 
 if __name__ == '__main__':
     app.secret_key = '*87gas6&*(73()fa98Nla&$62Nv%#{az' #Secret key for sessions | This key HAS TO BE CHANGED IN THE FINAL VERSION (and not being published on GitHub)
